73a4389e879308b8b1920e788b42532e3d2fbe84
Mohas <m.assar2002@gmail.com>
2020-09-27 14:52:47 +0330

    Many code smell fixes

--- app.js
+++ app.js
@@ -2,67 +2,45 @@
  * Copyright (c) 2020 | Mathana Kumar S (https://capturemathan.github.io)
  */
 import 'regenerator-runtime/runtime'
-const utils= require('./utils.js')
-const inithtml='<div class="column is-one-quarter"><div class="card" id="cardfolio">'
-const endhtml ='</div></div>'
-var finalhtml=inithtml;
+import getPortfolio from './utils.js'
 
-async function fetchFolio(token, username){
-    return utils(token, username).then(function(res){
-        return htmlFolio(res,username);
-    });
+async function fetchFolio(token, username) {
+    const result = getPortfolio(token, username)
+
+    const markup = `
+    <div class="column is-one-quarter">
+        <div class="card" id="cardfolio">
+            <div class="card-image" id="profilepic">
+                <figure class="image container is-128x128">
+                    <img class="is-rounded" src="${result['imgurl']}" />
+                </figure>
+            </div>
+            <div class="has-text-centered" id="name">
+                <a href="https://github.com/${username}" target="_blank">${result['name']}</a>
+            </div>
+            <div class="has-text-centered" id="languages">
+                ${result['languages'].map((lan) => `<span class="is-info tag" id="item">${lan}</span>`)}
+            </div>
+            <div class="has-text-centered" id="repos">
+                <img style="vertical-align:middle" src="https://raw.githubusercontent.com/capturemathan/Gitcard/master/assets/images/cloud.png" height="40" width="40" />
+                <span id="repotext">${result['repocount']} Repositories</span>
+            </div>
+            <div class="has-text-centered" id="contributions">
+                <img style="vertical-align:middle" src="https://raw.githubusercontent.com/capturemathan/Gitcard/master/assets/images/graph.png" height="40" width="40">
+                <span id="contribtext">${result['contributions']} Contributions</span>
+            </div>
+            <footer class="card-footer" id="follow">
+                <div class="card-footer-item">${result['followers']} Followers</div>
+                <div class="card-footer-item">${result['following']} Following</div>
+            </footer>
+        </div>
+    </div>
+`
+
+    return markup
 }
 
-function htmlFolio(result,username){
-    
-        //console.log(result)
-
-        const initprofile = '<div class="card-image" id="profilepic"><figure class="image container is-128x128"><img class="is-rounded"' 
-        + ' src=' + '"' + result["imgurl"] + '">';
-        const endprofile='</figure></div>';
-        finalhtml+=initprofile+endprofile;
-
-        const initname='<div class="has-text-centered" id="name"><a'
-        + ' href='+ '"https://github.com/' + username + '" ' + 'target="_blank">' + result["name"];
-        const endname= '</a></div>';
-        finalhtml+=initname+endname;
-
-        const initlang = '<div class="has-text-centered" id="languages">'
-        const endlang = '</div>'
-        const initspantag = '<span class="is-info tag" id="item">'
-        const endspantag = '</span>'
-        const langarray=result["languages"]
-        var finalspan=''
-        for(var i=0;i<langarray.length;i++){
-            finalspan+=initspantag+langarray[i]+endspantag;
-        }
-        finalhtml+=initlang+finalspan+endlang;
-
-        const initrepo='<div class="has-text-centered" id="repos"><img style="vertical-align:middle"' 
-        + ' src= ' +'"https://raw.githubusercontent.com/capturemathan/Gitcard/master/assets/images/cloud.png" ' + 'height="40" width="40">';
-        const nrepo = '<span id="repotext">' + result["repocount"] + ' Repositories';
-        const endrepo = '</span></div>';
-        finalhtml+=initrepo+nrepo+endrepo;
-
-        const initcontrib='<div class="has-text-centered" id="contributions"><img style="vertical-align:middle"' 
-        + ' src= ' +'"https://raw.githubusercontent.com/capturemathan/Gitcard/master/assets/images/graph.png" ' + 'height="40" width="40">';
-        const ncontrib = '<span id="contribtext">' + result["contributions"] + ' Contributions';
-        const endcontrib = '</span></div>';
-        finalhtml+=initcontrib+ncontrib+endcontrib;
-
-        const initfooter ='<footer class="card-footer" id="follow"><div class="card-footer-item">'
-        + result["followers"] + ' Followers</div><div class="card-footer-item">'
-        + result["following"] + ' Following</div></footer>';
-        finalhtml+=initfooter;
-
-        finalhtml+=endhtml;
-        //console.log(finalhtml)
-        return finalhtml
+module.exports = async function Gitcard(token, username) {
+    return await fetchFolio(token, username)
 }
-
-module.exports = async function Gitcard(token, username){
-    return fetchFolio(token, username).then(function(ans){
-        return ans;
-    });
-}
-//console.log(Gitcard('capturemathan'))
+console.log(Gitcard('bf3d6389af8f2cce57231d6392536371f535b33e','capturemathan'))
