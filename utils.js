bc6ec7d58c9070453a484947d6e8885c58a0da44
Mohas <m.assar2002@gmail.com>
2020-10-16 16:28:01 +0330

    rewrite with Templating and ES6 standards (#9)

--- utils.js
+++ utils.js
@@ -3,95 +3,54 @@
  */
 
 // Required Imports
-const fetch = require('node-fetch');
-
-// Variable Declarations
-var languages=[]
-var freqLanguages={}
-var folio={}
+const fetch = require('node-fetch')
 
 // Basic Details Function
-async function basicDetails(token, username){
-    await fetch('https://api.github.com/users/'+username)
-    .then(response => response.json())
-    .then(json => {
-        folio["name"]=json["name"]
-        folio["repocount"]=json["public_repos"]
-        folio["followers"]=json["followers"]
-        folio["following"]=json["following"]
-        folio["imgurl"]=json["avatar_url"]
-        folio["reposurl"]=json["repos_url"]
-    })
-    return topLanguages(token, username);
+async function getPortfolio(token, username) {
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    let data = await response.json()
+
+    data.languages = await topLanguages(data.repos_url)
+    data.contributions = await totalContributions(token, username)
+
+    return data
 }
 
 // Fetching Top 3 Languages
-async function topLanguages(token, username){
-    await fetch(folio["reposurl"])
-    .then(response => response.json())
-    .then(json => {
-        for(var i=0; i<json.length;i++){
-            var lang=json[i]["language"]
-            if(lang==null){
-                continue;
-            }
-            else if(freqLanguages[lang]){
-                freqLanguages[lang]++;
-            }
-            else{
-                freqLanguages[lang]=1
-            }
-        }
-        
-        var items = Object.keys(freqLanguages).map(function(key) {
-            return [key, freqLanguages[key]];
-        });
+async function topLanguages(reposurl) {
+    const response = await fetch(reposurl)
+    const data = await response.json()
 
-        items.sort(function(first, second) {
-            return second[1] - first[1];
-        });
-        
-        var count=0
-        for(var i=0;i<items.length;i++){
-            if(count==3){
-                break;
-            }
-            else{
-                languages.push(items[i][0])
-                count++;
-            }
-        }
-        folio["languages"]=languages
-    })
-    return totalContributions(token, username);
+    let languagesMap = data.reduce((acc, { language }) => ((acc[language] = (acc[language] || 0) + 1), acc), {})
+
+    let topThreeLanguages = Object.keys(languagesMap).filter(lang => lang !== 'null')
+        .sort((first, second) => languagesMap[second] - languagesMap[first])
+        .slice(0, 3)
+
+    return topThreeLanguages
 }
 
 // Fetching Total Contributions
-async function totalContributions(token, username){
+async function totalContributions(token, username) {
     const headers = {
-        "Authorization": `bearer ${token}`,
+        Authorization: `bearer ${token}`,
+        'Content-Type': 'application/json'
     }
-    const body = {
-        "query": `query {
+    const body = JSON.stringify({
+        query: `query {
             user(login: "${username}") {
-              contributionsCollection {
-                contributionCalendar {
-                  totalContributions
+                contributionsCollection {
+                    contributionCalendar {
+                        totalContributions
+                    }
                 }
-              }
             }
-          }`
-    }
-    const response = await fetch("https://api.github.com/graphql", { method: "POST", body: JSON.stringify(body), headers });
-    const data = await response.json();
-    folio["contributions"] = data.data.user.contributionsCollection.contributionCalendar.totalContributions;
+        }`
+    })
 
-    return folio
+    const response = await fetch('https://api.github.com/graphql', { method: 'POST', body, headers })
+    const data = await response.json()
+    return data.data.user.contributionsCollection.contributionCalendar.totalContributions
 }
 
-module.exports = async function myfolio(token, username){
-    const ans = await basicDetails(token, username);
-    return ans
-}
-
-//basicDetails('capturemathan');
+module.exports = getPortfolio
