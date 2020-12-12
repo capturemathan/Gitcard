99f366d6e5b8e30bd3ff0c6983d953ef1b6a82f9
capturemathan <capturesmk@gmail.com>
2020-12-12 20:40:56 +0530

    Formats Code :recycle:

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
 /*
  * Copyright (c) 2020 | Mathana Kumar S (https://capturemathan.github.io)
  */
-const getPortfolio = require('./utils.js')
+const getPortfolio = require("./utils.js");
 
 async function fetchFolio(token, username) {
-    const result = await getPortfolio(token, username)
+  const result = await getPortfolio(token, username);
 
-    const markup = `
+  const markup = `
     <div class="columns">
     <div class="column is-one-quarter">
         <div class="card" id="cardfolio">
             <div class="card-image" id="profilepic">
                 <figure class="image container is-128x128">
-                    <img class="is-rounded" src="${result['avatar_url']}" />
+                    <img class="is-rounded" src="${result["avatar_url"]}" />
                 </figure>
             </div>
             <div class="has-text-centered" id="name">
-                <a href="https://github.com/${username}" target="_blank">${result['name']}</a>
+                <a href="https://github.com/${username}" target="_blank">${
+    result["name"]
+  }</a>
             </div>
             <div class="has-text-centered" id="languages">
-                ${result['languages'].map((lan) => `<span class="is-info tag" id="item">${lan}</span>`).join(' ')}
+                ${result["languages"]
+                  .map(
+                    (lan) => `<span class="is-info tag" id="item">${lan}</span>`
+                  )
+                  .join(" ")}
             </div>
             <div class="has-text-centered" id="repos">
                 <img style="vertical-align:middle" src="https://raw.githubusercontent.com/capturemathan/Gitcard/master/assets/images/cloud.png" height="40" width="40" />
-                <span id="repotext">${result['public_repos']} Repositories</span>
+                <span id="repotext">${
+                  result["public_repos"]
+                } Repositories</span>
             </div>
             <div class="has-text-centered" id="contributions">
                 <img style="vertical-align:middle" src="https://raw.githubusercontent.com/capturemathan/Gitcard/master/assets/images/graph.png" height="40" width="40">
-                <span id="contribtext">${result['contributions']} Contributions</span>
+                <span id="contribtext">${
+                  result["contributions"]
+                } Contributions</span>
             </div>
             <footer class="card-footer" id="follow">
-                <div class="card-footer-item">${result['followers']} Followers</div>
-                <div class="card-footer-item">${result['following']} Following</div>
+                <div class="card-footer-item">${
+                  result["followers"]
+                } Followers</div>
+                <div class="card-footer-item">${
+                  result["following"]
+                } Following</div>
             </footer>
         </div>
     </div>
     </div>
-`
+`;
 
-    return markup
+  return markup;
 }
 
 async function Gitcard(token, username) {
-    return await fetchFolio(token, username)
+  return await fetchFolio(token, username);
 }
 
 module.exports = Gitcard;
--- utils.js
+++ utils.js
@@ -3,41 +3,45 @@
  */
 
 // Required Imports
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 // Basic Details Function
 async function getPortfolio(token, username) {
-    const response = await fetch(`https://api.github.com/users/${username}`)
-    let data = await response.json()
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  let data = await response.json();
 
-    data.languages = await topLanguages(data.repos_url)
-    data.contributions = await totalContributions(token, username)
+  data.languages = await topLanguages(data.repos_url);
+  data.contributions = await totalContributions(token, username);
 
-    return data
+  return data;
 }
 
 // Fetching Top 3 Languages
 async function topLanguages(reposurl) {
-    const response = await fetch(reposurl)
-    const data = await response.json()
+  const response = await fetch(reposurl);
+  const data = await response.json();
 
-    let languagesMap = data.reduce((acc, { language }) => ((acc[language] = (acc[language] || 0) + 1), acc), {})
+  let languagesMap = data.reduce(
+    (acc, { language }) => ((acc[language] = (acc[language] || 0) + 1), acc),
+    {}
+  );
 
-    let topThreeLanguages = Object.keys(languagesMap).filter(lang => lang !== 'null')
-        .sort((first, second) => languagesMap[second] - languagesMap[first])
-        .slice(0, 3)
+  let topThreeLanguages = Object.keys(languagesMap)
+    .filter((lang) => lang !== "null")
+    .sort((first, second) => languagesMap[second] - languagesMap[first])
+    .slice(0, 3);
 
-    return topThreeLanguages
+  return topThreeLanguages;
 }
 
 // Fetching Total Contributions
 async function totalContributions(token, username) {
-    const headers = {
-        Authorization: `bearer ${token}`,
-        'Content-Type': 'application/json'
-    }
-    const body = JSON.stringify({
-        query: `query {
+  const headers = {
+    Authorization: `bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+  const body = JSON.stringify({
+    query: `query {
             user(login: "${username}") {
                 contributionsCollection {
                     contributionCalendar {
@@ -45,12 +49,17 @@ async function totalContributions(token, username) {
                     }
                 }
             }
-        }`
-    })
+        }`,
+  });
 
-    const response = await fetch('https://api.github.com/graphql', { method: 'POST', body, headers })
-    const data = await response.json()
-    return data.data.user.contributionsCollection.contributionCalendar.totalContributions
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    body,
+    headers,
+  });
+  const data = await response.json();
+  return data.data.user.contributionsCollection.contributionCalendar
+    .totalContributions;
 }
 
 module.exports = getPortfolio;
